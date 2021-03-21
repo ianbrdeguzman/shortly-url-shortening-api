a7f8472d7423d77a9ebd22e99babe0c94bc03134
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-03-20 22:47:32 -0400

    fixed API.js

--- js/API.js
+++ js/API.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:js/Api.js
 import UI from './UI.js';
 
 class API {
@@ -24,31 +23,3 @@ class API {
 const ui = new UI();
 
 export default API;
-=======
-//
-import UI from './UI.js';
-
-class API {
-    async getLink(input) {
-        try {
-            const url = `https://api.shrtco.de/v2/shorten?url=${input}`;
-            const response = await fetch(url);
-            const data = await response.json();
-            if (!data.ok) {
-                const error = new Error(data.error);
-                throw error;
-            } else {
-                return data;
-            }
-        } catch (err) {
-            alert(err);
-            ui.toggleloading();
-            ui.resetForm();
-        }
-    }
-}
-
-const ui = new UI();
-
-export default API;
->>>>>>> 77a08e8a974ec16a707e88acb6cb13ca0f913f0b:js/API.js
